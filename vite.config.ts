a47f284d42b12baeb6ed04bec01abde02ce2a2d6
POOJA SAINI <pooja.saini12@gmail.com>
2026-04-27 17:15:09 +0530

    fix: added base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/pooja-react-portfolio/",   // 👈 YE LINE ADD KARO
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
 })
-
-
